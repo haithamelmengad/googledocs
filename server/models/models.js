57ea01dc9e8435d02ae69e050e4b6240dbc39ada
Delaney Ugelstad <dugelsta@wellesley.edu>
2018-03-21 19:34:22 -0700

    fixed routes for saving and versioning document

--- server/models/models.js
+++ server/models/models.js
@@ -36,6 +36,7 @@ const documentSchema = new Schema({
   //   type: mongoose.Schema.Types.ObjectId,
   //   ref: 'User',
   // }],
+  currentContent: Object,
   versions: [Object],
 });
 const Document = mongoose.model('Document', documentSchema);
